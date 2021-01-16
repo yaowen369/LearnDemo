22542ed508d4dae8b2fc5bb0949d4bd29fedbd25
yaowen <Wen.Yao2@geely.com>
2021-01-16 22:19:16 +0800

    处理编译版本等

--- app/src/main/java/com/yaoxiaowen/learndemo/MainActivity.java
+++ app/src/main/java/com/yaoxiaowen/learndemo/MainActivity.java
@@ -1,13 +1,11 @@
 package com.yaoxiaowen.learndemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.yaoxiaowen.learndemo.util.LogUtil;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     public static final String TAG = "MainActivity";
 
