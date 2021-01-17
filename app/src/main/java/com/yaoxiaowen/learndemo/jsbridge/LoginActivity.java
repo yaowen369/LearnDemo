5ea314902414df6aec30fdba53d8c130b86a8936
yaowen <Wen.Yao2@geely.com>
2021-01-17 11:44:59 +0800

    demo完善

--- app/src/main/java/com/yaoxiaowen/learndemo/jsbridge/LoginActivity.java
+++ app/src/main/java/com/yaoxiaowen/learndemo/jsbridge/LoginActivity.java
@@ -167,8 +167,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
+        if (TextUtils.isEmpty(password) ) {
+            mPasswordView.setError("密码为null");
             focusView = mPasswordView;
             cancel = true;
         }
@@ -176,10 +176,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         // Check for a valid email address.
         if (TextUtils.isEmpty(email)) {
             mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
-            cancel = true;
-        } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
             focusView = mEmailView;
             cancel = true;
         }
@@ -197,15 +193,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         }
     }
 
-    private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
-        return email.contains("@");
-    }
-
-    private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
-    }
 
     /**
      * Shows the progress UI and hides the login form.
